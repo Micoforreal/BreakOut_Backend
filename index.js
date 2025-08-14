5d3e98adb5c41209046e089fea9273fd18ccd830
micoforreal <mjjames006@gmail.com>
2025-08-14 03:31:44 +0100

    updated

--- index.js
+++ index.js
@@ -14,18 +14,23 @@ const {
   createEdgeClient,
   RewardKind,
   MintAsKind,
+  ResourceStorageEnum,
 } = require("@honeycomb-protocol/edge-client");
 const { profile } = require("console");
+const { BN } = require("bn.js");
 
 const API_URL = "https://edge.test.honeycombprotocol.com/";
 
 const PROJECT_ADDRESS = "8Myc9f4fBVT3MNPobuV4CssNBBBDD5jmhzguwvArrZAY";
-
+const RESOURCE_ADDRESS = "RF1giWg7uJ3y5Jo75TESDdwD3XWNZCL6t4jftkmkaKF";
 const CHARACTER_ASSEMBLER_ADDRESS =
-  "FXoeEU1T6FCP7SSNNGVbKjkkaPCngU9e4i2fWxw2fzM8";
+  "H3S7gzfi4u8GYvN4EqiaGjzGhLvacTaZvMkmfyZYYWkx";
 
-const CHARACTER_MODEL_ADDRESS = "C5yMSkEAfd7CEGni5kAFJXbuRbeUGHAX31toRaDsB7AH";
+const CHARACTER_MODEL_ADDRESS = "HiEqSXeRyVELBUvo8oMsSkipQuDqqNYbQUeqK6ivSiJN";
 
+const MISSION_POOL_ADDRESS = "9FQCF5Hupe4kibhqvFS92XBoYQDAp32UhJbEoaLhXqa";
+
+const MISSION_ADDRESS = "E7jVyKaiKYW3zEcyJQzrdmRukQXVvAT8W2ZquWrDnDnM";
 const client = createEdgeClient(API_URL, true);
 
 // import createEdgeClient from "@honeycomb-protocol/edge-client";
@@ -112,13 +117,11 @@ app.post("/createCharacter", async (req, res) => {
       authority: adminPublicKey.toString(),
 
       treeConfig: {
-        // This tree is used to store character traits and their necessary information
-        // Provide either the basic or advanced configuration, we recommend using the basic configuration if you don't know the exact values of maxDepth, maxBufferSize, and canopyDepth (the basic configuration will automatically configure these values for you)
         basic: {
           numAssets: 1000, // The desired number of character information this tree will be able to store
         },
       },
-      ticker: "new-character", // Provide a unique ticker for the config (the ticker ID only needs to be unique within the project)
+      ticker: "new_character", // Provide a unique ticker for the config (the ticker ID only needs to be unique within the project)
     });
 
     // Sign the transaction
@@ -132,7 +135,9 @@ app.post("/createCharacter", async (req, res) => {
     } = await client.createCreateCharacterModelTransaction({
       project: PROJECT_ADDRESS.toString(),
       authority: adminPublicKey.toString(),
-      payer: adminPublicKey.toString(), // Optional, use this if you want a different wallet to pay the transaction fee, by default the authority pays for this tx
+      payer: adminPublicKey.toString(),
+
+      // Optional, use this if you want a different wallet to pay the transaction fee, by default the authority pays for this tx
       mintAs: {
         // Optional, you can define the underlying protocol, default is MplCore
         kind: MintAsKind.MplCore,
@@ -141,9 +146,9 @@ app.post("/createCharacter", async (req, res) => {
         kind: "Assembled",
         assemblerConfigInput: {
           assemblerConfig: assemblerConfigAddress.toString(),
-          collectionName: "OutBreak collection",
+          collectionName: "OutBreak characters",
           name: "captain jack",
-          symbol: "NOBCJ",
+          symbol: "NOBC",
           description: "This is a character model for the OutBreak project",
           sellerFeeBasisPoints: 0,
           creators: [
@@ -156,30 +161,30 @@ app.post("/createCharacter", async (req, res) => {
       },
     });
 
-    await signTransaction(modelTx);
+    const signedTransaction = await signTransaction(modelTx);
 
-    const {
-      createCreateCharactersTreeTransaction: {
-        treeAddress: charactersTreeAddress, // The address of the character model, this is the address that will be used to create characters
-        tx: treeTx, // The transaction response, you'll need to sign and send this transaction
-      },
-    } = await client.createCreateCharactersTreeTransaction({
-      authority: adminPublicKey.toString(),
-      project: PROJECT_ADDRESS.toString(),
-      characterModel: characterModelAddress.toString(),
-      treeConfig: {
-        basic: {
-          numAssets: 1000,
-        },
-      },
-    });
+    // const {
+    //   createCreateCharactersTreeTransaction: {
+    //     treeAddress: charactersTreeAddress, // The address of the character model, this is the address that will be used to create characters
+    //     tx: treeTx, // The transaction response, you'll need to sign and send this transaction
+    //   },
+    // } = await client.createCreateCharactersTreeTransaction({
+    //   authority: adminPublicKey.toString(),
+    //   project: PROJECT_ADDRESS.toString(),
+    //   characterModel: characterModelAddress.toString(),
+    //   treeConfig: {
+    //     basic: {
+    //       numAssets: 1000,
+    //     },
+    //   },
+    // });
 
     // Sign the transaction
-    const signedTransaction = await signTransaction(treeTx);
+    // const signedTransaction = await signTransaction(treeTx);
 
     res.status(200).send({
       characterModelAddress: characterModelAddress.toString(),
-      charactersTreeAddress: charactersTreeAddress.toString(),
+      // charactersTreeAddress: charactersTreeAddress.toString(),
       assemblerConfigAddress: assemblerConfigAddress.toString(),
       transactionSignature: signedTransaction[0].responses[0].signature,
     });
@@ -192,15 +197,11 @@ app.post("/createCharacter", async (req, res) => {
 app.post("/userCharacter", async (req, res) => {
   const { userPublicKey } = req.body; // Get the user public key from the request body
 
-  // const {
-    
-  //   createAssembleCharacterTransaction: txResponse, 
+  const {
+    createAssembleCharacterTransaction: txResponse,
 
-  //   // This is the transaction response you'll need to sign and send
-  // } 
-  
-  const response
-  = await client.createAssembleCharacterTransaction({
+    // This is the transaction response you'll need to sign and send
+  } = await client.createAssembleCharacterTransaction({
     project: PROJECT_ADDRESS.toString(), // Project public key as a string
     authority: adminPublicKey.toString(), // Authority public key as a string
     assemblerConfig: CHARACTER_ASSEMBLER_ADDRESS.toString(), // Assembler config address as a string
@@ -209,53 +210,139 @@ app.post("/userCharacter", async (req, res) => {
     uri: "https://gateway.pinata.cloud/ipfs/bafybeiflcrsab4h5e5nfc4vfl2bag7qfb7svnnnp3lqtswejm62fkqtecm",
   });
 
-  // const response = await signTransaction(txResponse);
+  const response = await signTransaction(txResponse);
 
   res
     .status(200)
     .send({ success: true, message: "Character Assembled", response });
 });
 
+app.post("/testMission", async (req, res) => {
+  // const { userPublicKey } = req.body;
+
+  // const a = 846;           // number
+  // const b = BN(846);
 
 
 
-const createMission = async () => {
-  // const {
-  //   createCreateMissionPoolTransaction: {
-  //     missionPoolAddress, // The address of the mission pool
-  //     tx, // The transaction response, you'll need to sign and send this transaction
-  //   },
-  // } = await client.createCreateMissionPoolTransaction({
-  //   data: {
-  //     name: "Test Mission Pool",
-  //     project: projectAddress.toString(),
-  //     payer: adminPublicKey.toString(),
-  //     authority: adminPublicKey.toString(),
-  //     characterModel: CHARACTER_MODEL_ADDRESS.toString(),
-  //   },
-  // });
 
-  const {
-    createCreateMissionTransaction: {
-      missionAddress, // The address of the mission
-      tx: txResponse, // The transaction response, you'll need to sign and send this transaction
-    },
-  } = await client.createCreateMissionTransaction({
+
+  const response = await client.createSendCharactersOnMissionTransaction({
     data: {
-      name: "Test mission",
-      project: PROJECT_ADDRESS.toString(),
-      cost: {
-        address: resourceAddress.toString(),
-        amount: "1",
-      },
-      duration: "86400", // 1 day
-      minXp: "1", // Minimum XP required to participate in the mission
-      
+      userId: 846,
+      mission: MISSION_ADDRESS.toString(),
+      characterAddresses: ["GdGi7yorx1dyCXRWpkHy9rVf5KhvhTkEtEmTdu9mepr8"],
       authority: adminPublicKey.toString(),
 
+      payer: adminPublicKey.toString(), // Optiona
     },
   });
-};
+
+
+
+
+
+
+  
+
+  // await client.findCharacters({
+
+  //   // "6pWVH44Lcd41CD14BRWjhscrfQjd73AY45NBfK4ZuUMM"
+  //   addresses: [], // String array of character addresses
+  //   includeProof: true,
+  //   filters: {}, // Available filters are usedBy, owner, and source
+  //   mints: [], // Array of NFT mint public keys as a string
+  //   trees: [], // Array of character model merkle tree public keys as a string
+  //   wallets: ["3ykeDCgBc1mA983PN12pdL9f3JhBWXsEkjwXDhFwzuw9"], // Array of wallet public keys as a string (wallets that own the characters)
+  //   attributeHashes: [] // Array of attribute hashes as a string
+  // });
+
+  res.status(200).send({
+    response: response,
+  });
+});
+
+app.post("/createResource", async (req, res) => {
+  const {
+    createCreateNewResourceTransaction: {
+      resource: resourceAddress, // This is the resource address once it'll be created
+      tx: txResponse, // This is the transaction response, you'll need to sign and send this transaction
+    },
+  } = await client.createCreateNewResourceTransaction({
+    project: PROJECT_ADDRESS.toString(),
+    authority: adminPublicKey.toString(),
+    params: {
+      name: "Gold",
+      decimals: 2, // Number of decimal places the resource can be divided into
+      symbol: "GOLD", // Symbol of the resource
+      uri: "https://gateway.pinata.cloud/ipfs/bafkreifviodz6mglbjunbgdy5zpfkgr5scf36uumpsc5by2zkhvbsqr5ri", // URI of the resource
+      storage: ResourceStorageEnum.LedgerState, // Type of the resource, can be either AccountState (uncompressed/unwrapped) or LedgerState (compressed/wrapped)
+    },
+  });
+
+  const signedTransaction = await signTransaction(txResponse);
+
+  res.status(200).send({
+    resourceAddress: resourceAddress.toString(),
+    transaction: signedTransaction[0].responses,
+  });
+});
+
+app.post("/createMissions", async (req, res) => {
+  try {
+    // const {
+    //   createCreateMissionPoolTransaction: { missionPoolAddress, tx },
+    // } = await client.createCreateMissionPoolTransaction({
+    //   data: {
+    //     name: "Test Mission Pool",
+    //     project: PROJECT_ADDRESS.toString(),
+    //     payer: adminPublicKey.toString(),
+    //     authority: adminPublicKey.toString(),
+    //     characterModel: CHARACTER_MODEL_ADDRESS.toString(),
+    //   },
+    // });
+
+    //  await signTransaction(tx);
+
+    const {
+      createCreateMissionTransaction: { missionAddress, tx: txResponse },
+    } = await client.createCreateMissionTransaction({
+      data: {
+        name: "test mission4",
+        project: PROJECT_ADDRESS.toString(),
+
+        cost: {
+          address: RESOURCE_ADDRESS.toString(),
+          amount: "0",
+        },
+        duration: "0", // 1 day
+        minXp: "0",
+        rewards: [
+          {
+            kind: RewardKind.Xp,
+            max: "10",
+            min: "10",
+          },
+        ],
+
+        missionPool: MISSION_POOL_ADDRESS.toString(),
+        authority: adminPublicKey.toString(),
+        payer: adminPublicKey.toString(),
+      },
+    });
+
+    const signedTransaction = await signTransaction(txResponse);
+
+    res.status(200).send({
+      // missionPoolAddress: missionPoolAddress.toString(),
+      missionAddress: missionAddress.toString(),
+      transaction: signedTransaction[0].responses,
+    });
+  } catch (error) {
+    console.error("Error creating mission:", error);
+    res.status(500).send({ error: error.message });
+  }
+});
 
 const createProfileTree = async (projectAddress) => {
   try {
@@ -303,23 +390,18 @@ app.post("/addAchievement", async (req, res) => {
 
   const response = await signTransaction(txResponse);
 
-  res
-    .status(200)
-    .send({
-      success: true,
-      message: "Achievement added successfully",
-      response,
-    });
+  res.status(200).send({
+    success: true,
+    message: "Achievement added successfully",
+    response,
+  });
 });
 
 app.get("/", async (req, res) => {
   // const response= await createProject()
-
   // const ress = await createProject();
-
   // console.log("Project created:", ress);
   // const response = await createProfileTree(ress.projectAddress);
-
   // res.send({ w: ress, e: response });
 });
 
